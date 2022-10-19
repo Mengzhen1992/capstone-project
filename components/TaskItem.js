0b422476e93604cd7dcdc7e9821bfd14d239f5f8
Mengzhen Liao <liaomengzhen@gmail.com>
2022-10-19 12:24:33 +0200

    add svg files into images and add uncheck button

--- components/TaskItem.js
+++ components/TaskItem.js
@@ -1,8 +1,13 @@
 import styled from "styled-components";
+import uncheckButton from "../public/images/uncheck.svg";
+import Image from "next/image";
 
 const TaskItem = ({ taskName, duration }) => {
   return (
     <Item>
+      <ImageContainer>
+        <Image src={uncheckButton} alt="uncheck button of a task item" />
+      </ImageContainer>
       <TaskName>{taskName}</TaskName>
       <TaskDuration>{duration}</TaskDuration>
     </Item>
@@ -12,6 +17,7 @@ const TaskItem = ({ taskName, duration }) => {
 export default TaskItem;
 
 const Item = styled.li`
+  position: relative;
   background: rgba(255, 255, 255, 0.5);
   box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.1);
   border-radius: 8px;
@@ -19,6 +25,12 @@ const Item = styled.li`
   padding-left: 4rem;
   padding-top: 0.8rem;
   padding-bottom: 0.8rem;
+`;
+
+const ImageContainer = styled.div`
+  position: absolute;
+  left: 1.5rem;
+  top: 1.5rem;
 `;
 
 const TaskName = styled.p`
